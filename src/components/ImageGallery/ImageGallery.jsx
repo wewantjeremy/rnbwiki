91e31bc35e08e568366a57e45a49bcdc09fbd1c0
wewantjeremy <dcmwise@gmail.com>
2026-09-02 03:25:59 -0400

    Load artist images from R2

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import styles from './ImageGallery.module.css'
 
 export default function ImageGallery( {images} ){
+  console.log("IMAGEGALLERY IMAGES:", images);
   const [slide, setSlide] = useState(0);
   return(
 <div className={styles.galleryContainer}>
-  <img className={styles.galleryImage} src={images[slide]} />
-
+<img
+  className={styles.galleryImage}
+  src={encodeURI(
+    `https://images.rnbwiki.com${images[slide].replace("/src/images", "")}`
+  )}
+/>
   <button
     className={`${styles.arrow} ${styles.prev}`}
     onClick={() => setSlide((slide + images.length - 1) % images.length)}
